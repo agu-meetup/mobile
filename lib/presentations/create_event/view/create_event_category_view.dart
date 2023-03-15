46e9e8815defb283cc533cf26869af25a7abc9dd
Kerem Keskin <keskinkrm55@gmail.com>
2023-03-15 17:05:35 +0300

    Some changes on create event page

---
+++ lib/presentations/create_event/view/create_event_category_view.dart
@@ -0,0 +1,43 @@
+import 'package:agu_meetup_mobile/presentations/create_event/model_view/create_event_model_view.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../../components/my_dropdowns/my_dropdown_widget.dart';
+import '../../../core/constants.dart';
+
+class CreateEventCategoryView extends StatelessWidget {
+  const CreateEventCategoryView({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<CreateEventModelView>(
+      builder: (context, mv, child) {
+        return Column(
+          children: [
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'CATEGORY',
+                style: TextStyle(
+                  fontSize: 12,
+                  color: kDarkGray,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+            const SizedBox(height: 4),
+            MyDropdownWidget(
+              dropdownValue: mv.categoryDropdownValue,
+              elementList: mv.categoryList,
+              onChangedFunc: mv.changeSelectedCategory,
+              isThereShadow: false,
+              hintText: "Category",
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
